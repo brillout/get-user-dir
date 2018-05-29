3270f6794f08f4ff26591257b537ddd6426d7e93
Romuald Brillout <github.com@brillout.com>
2018-05-29 15:22:04 +0200

    increase stack trace size

--- getUserDir.js
+++ getUserDir.js
@@ -1,23 +1,39 @@
 const pathModule = require('path');
-const callsites = require('callsites');
+const assert_internal = require('reassert/internal');
+
+const GLOBAL_KEY = '__@brillout/get-user-dir__userDir';
+
 
 module.exports = getUserDir;
 module.exports.setUserDir = setUserDir;
 module.exports.userDir = null;
 
-const globalKey = '__@brillout/get-user-dir__userDir';
 
 function getUserDir() {
-    if( global[globalKey] ) {
-        return global[globalKey];
+    if( global[GLOBAL_KEY] ) {
+        return global[GLOBAL_KEY];
     }
-    const stacks = callsites();
-    for(let i = stacks.length-1; i>=0; i--) {
-        const stack = stacks[i];
-        if( stack.isNative() ) {
+
+    const firstCall = getFirstUserLandCall();
+    if( firstCall ) {
+        return firstCall;
+    }
+
+    return process.cwd();
+}
+
+function setUserDir(userDir) {
+    global[GLOBAL_KEY] = userDir;
+}
+
+function getFirstUserLandCall() {
+    const calls = getV8StackTrace();
+    for(let i = calls.length-1; i>=0; i--) {
+        const call = calls[i];
+        if( call.isNative() ) {
             continue;
         }
-        const filePath = stack.getFileName();
+        const filePath = call.getFileName();
         if( ! filePath ) {
             continue;
         }
@@ -25,20 +41,27 @@ function getUserDir() {
             continue;
         }
         if( ! isDependency(filePath) ) {
-            return pathModule.dirname(filePath);
+            const userDir = pathModule.dirname(filePath);
+            assert_internal(userDir && pathModule.isAbsolute(userDir));
+            return userDir;
         }
         break;
     }
-    return process.cwd();
+    return null;
 }
-
-function setUserDir(userDir) {
-    global[globalKey] = userDir;
-}
-
 function isNode(filePath) {
     return !pathModule.isAbsolute(filePath);
 }
 function isDependency(filePath) {
     return filePath.split(pathModule.sep).includes('node_modules');
+}
+function getV8StackTrace() {
+    const callsites = require('callsites');
+
+    const stackTraceLimit__original = Error.stackTraceLimit;
+    Error.stackTraceLimit = Infinity;
+    const calls = callsites();
+    Error.stackTraceLimit = stackTraceLimit__original;
+
+    return calls;
 }
